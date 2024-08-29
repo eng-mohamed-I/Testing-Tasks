0217602d59aeddf375485267d381a0d31136ac9b
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-08-29 14:55:40 +0300

    first tasl

--- Lab-Day1/index.js
+++ Lab-Day1/index.js
@@ -22,8 +22,8 @@ const createArray = (number) => {
   const myArray = Array.from(Array(number).keys());
   return myArray;
 };
-
-module.exports = { capitalizeText, createArray };
+export { capitalizeText, createArray };
+// module.exports = { capitalizeText, createArray };
 
 //test that the return value of type array
 //test if we pass 3 it will return array of length 3 and test it's include 1
--- Lab-Day1/test/index.test.js
+++ Lab-Day1/test/index.test.js
@@ -1,74 +1,50 @@
-const { capitalizeText, createArray } = require("../index");
-
-var assertMocha = require("assert");
-var expect = require("chai").expect;
-var should = require("chai").should;
-var assert = require("chai").assert;
+import { capitalizeText, createArray } from "../index.js";
+import assertMocha from "assert";
+import { expect, should, assert } from "chai";
 should();
 
-// Test that the function takes a string and returns a string
-// Test that the function takes a string and returns it after capitalizing it
-// Test that if the function takes a number, it throws a TypeError saying "parameter should be string"
-// Test that if the input is "iti", the returned value will not equal "hello"
-
+// Testing problem 1
 describe("capitalizeText", function () {
-  it("should return a string when given a string", function () {
+  it("output should be string when put string", function () {
     expect(capitalizeText("iti")).to.be.a("string");
   });
 
   it("should return the string capitalized", function () {
-    assertMocha.equal(capitalizeText("iti"), "Iti");
+    assertMocha.equal(capitalizeText("iti"), "ITI");
   });
 
   it("should throw a TypeError if the input is not a string", function () {
-    expect(() => capitalizeText(12)).to.throw(
+    expect(capitalizeText(90)).to.throw(
       TypeError,
       "parameter should be string"
     );
   });
 
-  it('should not return "hello" when the input is "iti"', function () {
+  it("should return the output dont like input", function () {
     expect(capitalizeText("iti")).to.not.equal("hello");
   });
 });
 
 // #####################################
-//create array
-// Problem
-// input number 3 ==> output should be [0,1,2]
-//test that the return value of type array
-//test if we pass 3 it will return array of length 3 and test it's include 1
-//make a variable and intialize it by 1 and before each test increase it by one and pass it to every function call
-//Bonus-->//try to delay the testing process 5 seconds
-//try to use different styles (expect , should , assert)
-//Bonus--> //after finishing your test process try to  run it on a browser
-//make pending test case
-
+// Tasting Problem 2
 describe("createArray", function () {
-  let variable = 1;
-
-  beforeEach(function () {
-    variable++;
-  });
-
   it("should return an array", function () {
-    expect(createArray(variable)).to.be.an("array");
+    expect(createArray(3)).to.be.an("array");
   });
 
-  it("should return an array of length equal to the input number", function () {
-    const result = createArray(variable);
-    result.should.have.lengthOf(variable);
+  it("input should = length of array", function () {
+    const result = createArray(3);
+    result.should.have.lengthOf(3);
   });
 
   it("should include the number 1 in the array", function () {
-    const result = createArray(variable);
-    assert.include(result, 1);
+    const result = createArray(3);
+    assert.include(result, 25);
   });
 
   it("should delay the testing process by 5 seconds", function (done) {
-    this.timeout(6000); // Set timeout to 6 seconds
     setTimeout(() => {
-      const result = createArray(variable);
+      const result = createArray(3);
       expect(result).to.include(1);
       done();
     }, 5000);
